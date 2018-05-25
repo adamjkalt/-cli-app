45110c5a000438d04e2e748cccefc4b27f7c72ac
Adam Kalt <adam.kalt.1@gmail.com>
2018-05-25 19:34:08 +0000

    update

---
+++ UpcomingNintendoSwitchGames/lib/UpcomingNintendoSwitchGames/cli.rb
@@ -0,0 +1,11 @@
+class UpcomingNintendoSwitchGames::CLI
+
+def call
+  Puts "Do You Want to See a List of the Upcoming Nintendo Switch Games?"
+  Puts "Choose the Number Corresponding to your choice."
+  Puts "1. See List of Games"
+  Puts "2. Quit"
+end
+
+
+end
