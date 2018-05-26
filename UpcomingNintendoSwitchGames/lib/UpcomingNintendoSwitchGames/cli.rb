484d646dd00fa9afa98860314d95a681855066ef
Adam Kalt <adam.kalt.1@gmail.com>
2018-05-26 18:00:13 +0000

    update

--- UpcomingNintendoSwitchGames/lib/UpcomingNintendoSwitchGames/cli.rb
+++ UpcomingNintendoSwitchGames/lib/UpcomingNintendoSwitchGames/cli.rb
@@ -5,7 +5,19 @@ def call
   puts "Choose the Number Corresponding to your choice."
   puts "1. See List of Games"
   puts "2. Quit"
+  # list games
 end
 
+def list_games
+  <<~HEREDOC
+  puts "Upcoming Nintendo Switch Games"
+  1. Yoku's Island Express
+  2. Harvest Moon: Light of Hope
+  3. Street Fighter: 30th Anniversary Collection
+  HEREDOC
+end
 
+def menu
+  puts "Choose the Number Corresponding to the Game of your choice."
+  puts "Enter 'QUIT' to quit this program"
 end
