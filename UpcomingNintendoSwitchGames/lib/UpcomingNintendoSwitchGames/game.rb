bda5f413e8bc00ab14951d00f159a687f21c78a6
Adam Kalt <adam.kalt.1@gmail.com>
2018-05-26 20:03:14 +0000

    update

--- UpcomingNintendoSwitchGames/lib/UpcomingNintendoSwitchGames/game.rb
+++ UpcomingNintendoSwitchGames/lib/UpcomingNintendoSwitchGames/game.rb
@@ -3,22 +3,30 @@ class UpcomingNintendoSwitchGames::Game
 attr_accessor :name, :release_date, :url, :developer, :genre, :rating, :summary
 
   def self.today
-    # I should return a bunch of instances of games
-
-    game_1 = self.new
-    game_1.name = "Yoku's Island Express"
-    game_1.release_date = "May 29"
-    game_1.url = "http://www.metacritic.com/game/switch/yokus-island-express"
-
-    game_2 = self.new
-    game_2.name = "Harvest Moon: Light of Hope"
-    game_2.release_date = "May 29"
-    game_2.url = "http://www.metacritic.com/game/switch/harvest-moon-light-of-hope"
-
-    game_3 = self.new
-    game_3.name = "Street Fighter: 30th Anniversary Collection"
-    game_3.release_date = "May 29"
-    game_3.url = "http://www.metacritic.com/game/switch/street-fighter-30th-anniversary-collection"
-
-    [game_1, game_2, game_3]
+    self.scrape_games
   end
+
+  def self.scrape_games
+    # go to metacritic, find game
+    # extrac the properties
+    # instantiate the game
+    # [games]
+  end
+
+
+  # game_1 = self.new
+  # game_1.name = "Yoku's Island Express"
+  # game_1.release_date = "May 29"
+  # game_1.url = "http://www.metacritic.com/game/switch/yokus-island-express"
+  #
+  # game_2 = self.new
+  # game_2.name = "Harvest Moon: Light of Hope"
+  # game_2.release_date = "May 29"
+  # game_2.url = "http://www.metacritic.com/game/switch/harvest-moon-light-of-hope"
+  #
+  # game_3 = self.new
+  # game_3.name = "Street Fighter: 30th Anniversary Collection"
+  # game_3.release_date = "May 29"
+  # game_3.url = "http://www.metacritic.com/game/switch/street-fighter-30th-anniversary-collection"
+  #
+  # [game_1, game_2, game_3]
