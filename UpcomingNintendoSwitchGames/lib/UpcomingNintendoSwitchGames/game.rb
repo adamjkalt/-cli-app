fd704184c07ea2bcc24f283651285363667620fb
Adam Kalt <adam.kalt.1@gmail.com>
2018-06-19 03:42:40 +0000

    update

--- UpcomingNintendoSwitchGames/lib/UpcomingNintendoSwitchGames/game.rb
+++ UpcomingNintendoSwitchGames/lib/UpcomingNintendoSwitchGames/game.rb
@@ -34,36 +34,33 @@ def initialize(name:, release_date:, url:, developer:, genre:, summary:)
   def self.scrape_metacritic
     doc = Nokogiri::HTML(open("http://www.metacritic.com/browse/games/release-date/coming-soon/switch/date",
     ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE, 'User-Agent' => 'safari'))
-    # games = []
+    games = []
     doc.css('div.product_wrap').each do |game|
-      # Below was originally just name, release_date, and url
-      self.name = game.css('div.product_title').text.gsub(/\s+/, " ")
-      self.release_date = game.css('li.stat.release_date').text.gsub(/\s+/, " ")
-      self.url = game.css('div.product_title a')[0]["href"]
-      # games << {name: name, release_date: release_date, url: url}
-      # originally just above shovelling into games.
-      self.save
+      name = game.css('div.product_title').text.gsub(/\s+/, " ")
+      release_date = game.css('li.stat.release_date').text.gsub(/\s+/, " ")
+      url = game.css('div.product_title a')[0]["href"]
+      games << {name: name, release_date: release_date, url: url}
+      # self.save
     end
-    # games
+    games
   end
 
 def self.scrape_game(game)
   url = "http://www.metacritic.com" + game.url
   profile = Nokogiri::HTML(open(url,
   ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE, 'User-Agent' => 'safari'))
-  # game_info = []
+  game_info = []
   profile.css('div.layout').each do |the_game|
-    # Below was originally just name, release_date, etc, etc.
-    self.name = the_game.css('div.product_title').text.gsub(/\s+/, " ")
-    self.release_date = the_game.css('li.summary_detail.release_data').text.gsub(/\s+/, " ")
-    self.developer = the_game.css('li.summary_detail.developer').text.gsub(/\s+/, " ")
-    self.genre = the_game.css('li.summary_detail.product_genre').text.gsub(/\s+/, " ")
-    self.summary = the_game.css('li.summary_detail.product_summary').text.gsub(/\s+/, " ")
-    # game_info << {name: name, release_date: release_date, developer: developer,
+    name = the_game.css('div.product_title').text.gsub(/\s+/, " ")
+    release_date = the_game.css('li.summary_detail.release_data').text.gsub(/\s+/, " ")
+    developer = the_game.css('li.summary_detail.developer').text.gsub(/\s+/, " ")
+    genre = the_game.css('li.summary_detail.product_genre').text.gsub(/\s+/, " ")
+    summary = the_game.css('li.summary_detail.product_summary').text.gsub(/\s+/, " ")
+    game_info << {name: name, release_date: release_date, developer: developer,
     genre: genre, summary: summary}
-    self.save
+    # self.save
 end
-# game_info
+game_info
 end
 
 end
